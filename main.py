48c7defe37fdf0a57c53f8bca0549c10308e35dd
Antonio Alonso <alloant@gmail.com>
2023-03-22 22:12:48 +0530

    errors

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ def get_ctr(folder):
     if folder[:7] == 'Mailbox':
         return t[8:]
     elif folder[:10] == '8.Mailbox-':
-        return t[11:]
+        return t[10:]
 
-with SynologyDrive("vInd1",PASS,"nas.prome.sg",dsm_version='7') as synd:
+
+with SynologyDrive("df-scr",PASS,"nas.prome.sg",dsm_version='7') as synd:
     tf = synd.get_teamfolder_info()
     
     year = datetime.today().strftime('%Y')
@@ -27,49 +28,94 @@ with SynologyDrive("vInd1",PASS,"nas.prome.sg",dsm_version='7') as synd:
     despacho = "/team-folders/Despacho/Inbox Despacho"
     archive = f"/team-folders/Aes Archive/ctr in {year}"
 
+    #despacho = "/mydrive/despacho"
+    #archive = "/mydrive/archive"
+
     wb = Workbook()
     ws = wb.active
 
-    ws.append(['No','Year','Ref','Date','Content','Dept','#of docs'])
-    
+    ws.append(['ctr','No','Year','Ref','Date','Content','Dept','#of docs'])
+   
+    cont = 0
     
     for t in tf:
-        #if t[:7] == 'Mailbox':
-        if 'Mailbox' in t:
+        if 'Mailbox gul' in t:
             ctr = get_ctr(t)
+            
+            try:
+                mbs = synd.list_folder(f"/team-folders/{t}")
+            except:
+                print(f'Cannot get folders from {t}')
+                break
 
-            mbs = synd.list_folder(f"/team-folders/{t}")
             for mb in mbs['data']['items']:
                 if mb['name'] in [f"{ctr} to cr",f"{ctr}-to-cr"]:
                     print(f"Checking {t}")
+                    p_path = synd.get_file_or_folder_info(f"{mb['display_path']}")['data']['permanent_link']
+                    h_path = f'=HYPERLINK("#dlink=/oo/r/{p_path}", "{mb["name"]}")'
+ 
+                    try:
+                        mail = synd.list_folder(mb['display_path'])
+                    except:
+                        print(f"Cannot get file list from {mb['name']}")
                     
-                    mail = synd.list_folder(mb['display_path'])
                     for m in mail['data']['items']:
+                        cont += 1
                         note = f"{mb['display_path']}/{m['name']}"
                         
-                        print("Copying note to despacho")
-                        synd.copy(note,f"{despacho}/{m['name']}")
+                        print(f"    Copying {m['name']} to despacho")
                         
-                        print("Moving note to archive")
-                        synd.move_path(note,archive)
+
+                        try:
+                            synd.copy(note,f"{despacho}/{m['name']}")
+                        except:
+                            print("Cannot copy files")
+                            ws.append([ctr,h_path,year,'',date,f"ERROR in {mb['display_path']}",'',''])
+                            break
                         
-                        print("Saving link in register")
-                        p_link = synd.get_file_or_folder_info(f'{archive}/{m["name"]}')['data']['permanent_link']
-                        #link = f"https://nas.prome.sg:5001/oo/r/{p_link}"
-                        h_link = f'=HYPERLINK("#dlink=/oo/r/{p_link}", "{m["name"]}")'
+                        print("    Moving {m['name']} to archive")
+                        
+                        
+                        try:
+                            synd.move_path(note,archive)
+                        except:
+                            print(f"Cannot move {m['name']}")
+                            ws.append([ctr,h_path,year,'',date,f"ERROR in {mb['display_path']}",'',''])
+                            break
+
+                        print("    Saving link in register")
 
 
-                        ws.append(['',year,h_link,date,'','',''])
+                        try:
+                            p_link = synd.get_file_or_folder_info(f'{archive}/{m["name"]}')['data']['permanent_link']
+                            h_link = f'=HYPERLINK("#dlink=/oo/r/{p_link}", "{m["name"]}")'
+                        except:
+                            print("Cannot get link")
+                            h_link = h_path
+                        
+
+                        ws.append([ctr,h_link,year,'',date,'','',''])
 
     
-    file = NamedTemporaryFile()
-    wb.save(file)
-    file.seek(0)
-    file.name = f"{date.replace('/','-')}-reg.xlsx"
+    if cont > 0:
+        file = NamedTemporaryFile()
+        wb.save(file)
+        file.seek(0)
+        file.name = f"{date.replace('/','-')}-reg.xlsx"
     
-    print("Creating register file")
-    ret_upload = synd.upload_file(file, dest_folder_path='/mydrive')
-    ret_convert = synd.convert_to_online_office(ret_upload['data']['display_path'],
+        print("Creating register file")
+        uploaded = True
+        try:
+            ret_upload = synd.upload_file(file, dest_folder_path='/mydrive')
+        except:
+            print("Cannot upload register")
+            wb.save(f"{date.replace('/','-')}-reg.xlsx")
+            uploaded = False
+
+        if uploaded:
+            try:
+                ret_convert = synd.convert_to_online_office(ret_upload['data']['display_path'],
                     delete_original_file=True,
                     conflict_action='autorename')
-
+            except:
+                print("Cannot convert file to Synology Office")
